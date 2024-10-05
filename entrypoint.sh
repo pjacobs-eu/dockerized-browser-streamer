d8e6407d36dd01808902d96d7c317b86a7db88e3
Philip Jacobs <pjacobs@MacBook-Pro-von-Philip.local>
2024-10-05 15:59:23 +0200

    Remove firefox, update base

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,8 +30,7 @@ pacmd load-module module-virtual-sink sink_name=v1  # Load a virtual sink as `v1
 pacmd set-default-sink v1  # Set the `v1` as the default sink device
 pacmd set-default-source v1.monitor  # Set the monitor of the v1 sink to be the default source
 
-# 3. Firefox
-./firefox.sh & # Or ./chrome.sh &
+./chrome.sh &
 sleep 0.5  # Wait a bit for firefox to start before moving on
 xdotool mousemove 1 1 click 1  # Move mouse out of the way so it doesn't trigger the "pause" overlay on the video tile
 
